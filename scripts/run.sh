8b1dde936b3a77eef23a68fffdd4cde2ce9e3a8e
Nick Novikov <nick.novikov@gmail.com>
2021-09-24 21:40:46 +0800

    scripts: Install the quicktemplate qtc tool

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,7 @@ set -e
 cd "$(dirname "$0")/../public"
 
 [ -x "$GOPATH/bin/gin" ]  ||  go install github.com/codegangsta/gin@latest
+[ -x "$GOPATH/bin/qtc" ]  ||  go install github.com/valyala/quicktemplate/qtc@latest
 
 export SUMBUR_CONFIG=sumbur-debug.yaml
 gin  -a 8000  -b sumbur-debug  -t ..  run
