a4d66fc6e16cc0ee63afd748b0fe2efdb14e7e0f
Nick Novikov <nick.novikov@gmail.com>
2021-10-03 04:26:15 +0800

    blog: Check if the selected tag exists

--- views/blog/blog.go
+++ views/blog/blog.go
@@ -3,9 +3,20 @@ package blog
 import (
 	"sumbur/db"
 	"sumbur/views"
+	"sumbur/views/http_errors"
 
+	"github.com/MakeNowJust/heredoc"
 	"github.com/savsgio/atreugo/v11"
 )
+
+var SQL_CHECK_TAGS = heredoc.Doc(`
+SELECT "tag"."tag"
+FROM "tag"
+INNER JOIN "article"  USING ("article_id")
+WHERE
+	($1 OR "article"."public") AND ("tag"."tag" = $2)
+LIMIT 1
+`)
 
 type Blog struct {
 	*views.BasePage
@@ -19,11 +30,15 @@ func BlogView(auth views.IAuth) atreugo.View {
 		db := db.Open()
 		defer db.Close()
 
-		tag := views.PathValue(ctx, "tag")
+		stag := views.PathValue(ctx, "tag")
+
+		if (stag != &views.EmptyString) && !db.Query(&SQL_CHECK_TAGS, auth.State(), stag).Get() {
+			return http_errors.NotFoundView(auth)(ctx)
+		}
 
 		data := Blog{
-			tags:     QueryTags(db, auth, tag),
-			articles: QueryArticles(db, auth, tag),
+			tags:     QueryTags(db, auth, stag),
+			articles: QueryArticles(db, auth, stag),
 		}
 
 		views.WritePage(ctx, &data, auth)
