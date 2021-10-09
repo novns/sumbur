ce70d1004ded922064bf59b4bcc9f640f22643fc
Nick Novikov <nick.novikov@gmail.com>
2021-10-09 21:27:46 +0800

    page: Use JQuery

--- views/page.qtpl.go
+++ views/page.qtpl.go
@@ -139,6 +139,9 @@ func StreamPage(qw422016 *qt422016.Writer, page HTML) {
 </footer>
 
 
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
 </body>
 
 
