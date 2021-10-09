0c7c9ad4e40aaf387d4e641bc7c22fc280365e82
Nick Novikov <nick.novikov@gmail.com>
2021-10-09 22:47:04 +0800

    article: Add a function for querying a single article

--- views/blog/article.go
+++ views/blog/article.go
@@ -1,14 +1,51 @@
 package blog
 
-import "time"
+import (
+	"sumbur/db"
+	"time"
+
+	"github.com/MakeNowJust/heredoc"
+)
+
+var SQL_ARTICLE = heredoc.Doc(`
+SELECT
+	"article"."article_id",
+	"article"."title",
+	"article"."body",
+	"article"."body_ht",
+	"article"."public",
+	"article"."created",
+	array_agg("tag"."tag"  ORDER BY "tag"."tag")  AS "tags"
+FROM "article"
+FULL OUTER JOIN "tag"  USING ("article_id")
+WHERE
+	"article"."article_id" = $1
+GROUP BY
+	"article"."article_id"
+`)
 
 type Article struct {
 	stag *string
 
 	article_id int
 	title      []byte
+	body       []byte
 	body_ht    []byte
 	public     bool
 	created    time.Time
 	tags       [][]byte
+}
+
+func (article *Article) QueryArticle(db *db.DB, article_id int) bool {
+	return db.
+		Query(SQL_ARTICLE, article_id).
+		Get(
+			&article.article_id,
+			&article.title,
+			&article.body,
+			&article.body_ht,
+			&article.public,
+			&article.created,
+			&article.tags,
+		)
 }
