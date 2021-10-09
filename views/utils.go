ca7f73be93717921a06eb90452d698c001f7471f
Nick Novikov <nick.novikov@gmail.com>
2021-10-09 22:44:58 +0800

    utils: Get an int number path argument

--- views/utils.go
+++ views/utils.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"net/url"
+	"strconv"
 
 	"github.com/savsgio/atreugo/v11"
 )
@@ -16,4 +17,18 @@ func PathValue(ctx *atreugo.RequestCtx, key string) *string {
 
 	result, _ := url.PathUnescape(value.(string))
 	return &result
+}
+
+func IntValue(ctx *atreugo.RequestCtx, key string) int {
+	value := ctx.UserValue(key)
+	if value == nil {
+		return 0
+	}
+
+	result, err := strconv.Atoi(value.(string))
+	if err != nil {
+		return 0
+	}
+
+	return result
 }
